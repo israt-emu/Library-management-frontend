a17947edb2c25c13c3ceb62aff6814cc057d2fd3
Tushar@8640 <imtantushar44@gmail.com>
2023-02-12 03:23:53 +0600

    borrowed book

--- src/components/tables/TopBorrowedBooksTable.js
+++ src/components/tables/TopBorrowedBooksTable.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const TopBorrowedBooksTable = ({data}) => {
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+const TopBorrowedBooksTable = ({ data }) => {
   return (
     <div>
       <div className="mt-6">
         <div className="flex justify-between items-center">
-          <h1 className="text-2xl font-medium mb-3">Top Five Borrowed Books:</h1>
+          <h1 className="text-2xl font-medium mb-3">
+            Top Five Borrowed Books:
+          </h1>
         </div>
-        <div className="text-gray-800" style={{height: "250px"}}>
+        <div className="text-gray-800" style={{ height: "250px" }}>
           <div className="overflow-x-auto">
             <table className="w-full p-6 text-sm text-left whitespace-nowrap">
               <thead>
@@ -24,7 +28,11 @@ const TopBorrowedBooksTable = ({data}) => {
                 {data?.map((d, i) => (
                   <tr key={i}>
                     <td className="px-3 py-2">
-                      <p>{d?.name}</p>
+                      <Link to={`/dashboard/books/${d?.bookId}`}>
+                        <div className="flex justify-center items-center space-x-1">
+                          <p>{d?.name}</p> <MdOutlineRemoveRedEye />
+                        </div>
+                      </Link>
                     </td>
                     <td className="px-3 py-2 text-center">
                       <p className="text-gray-600">{d?.category}</p>
