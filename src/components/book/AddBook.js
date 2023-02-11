cb9d36cdf1f65248b16c7916167eaa7e81b8d66e
Tushar@8640 <imtantushar44@gmail.com>
2023-02-11 12:36:30 +0600

    add book complete

--- src/components/book/AddBook.js
+++ src/components/book/AddBook.js
@@ -1,6 +1,25 @@
-import React from "react";
+import { data } from "autoprefixer";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useAddBookMutation } from "../../features/book/bookAPI";
 
 const AddBook = () => {
+  const { user } = useSelector((state) => state.auth);
+  const [addBook,{data,error,isLoading,isError}] = useAddBookMutation()
+  const [bookData, setBookData] = useState({});
+  const handleOnchange = (e) => {
+    bookData[e.target.name] = e.target.value;
+    setBookData({ ...bookData });
+  };
+  const handleSubmit = () => {
+    console.log(bookData);
+    bookData.addedBy = user?._id;
+    bookData.totalViews = 0;
+    bookData.totalBorrowed = 0;
+    console.log(bookData);
+    addBook(bookData)
+  };
+  console.log(data);
   return (
     <div>
       <section className="py-6 bg-gray-100 text-gray-900">
@@ -13,6 +32,8 @@ const AddBook = () => {
               <label className="block mb-1">
                 <span className="my-2">Book Title</span>
                 <input
+                  onChange={handleOnchange}
+                  name="name"
                   type="text"
                   placeholder="Book Title"
                   className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
@@ -21,7 +42,9 @@ const AddBook = () => {
               <label className="block mb-1">
                 <span className="my-2">Book Id</span>
                 <input
+                  onChange={handleOnchange}
                   type="text"
+                  name="bookId"
                   placeholder="Give an unique Id"
                   className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
                 />
@@ -29,7 +52,9 @@ const AddBook = () => {
               <label className="block mb-1">
                 <span className="my-2">Category</span>
                 <input
-                  type="number"
+                  onChange={handleOnchange}
+                  name="category"
+                  type="text"
                   placeholder="Give an unique Id"
                   className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
                 />
@@ -37,6 +62,8 @@ const AddBook = () => {
               <label className="block mb-1">
                 <span className="my-2">Description</span>
                 <textarea
+                  onChange={handleOnchange}
+                  name="description"
                   type="text"
                   placeholder="Description"
                   className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
@@ -46,6 +73,8 @@ const AddBook = () => {
               <label className="block mb-1">
                 <span className="my-2">Total Stock</span>
                 <input
+                  onChange={handleOnchange}
+                  name="totalStock"
                   type="number"
                   placeholder="Total stock"
                   className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
@@ -54,6 +83,8 @@ const AddBook = () => {
               <label className="block mb-1">
                 <span className="my-2">Book Location</span>
                 <input
+                  onChange={handleOnchange}
+                  name="bookLocation"
                   type="text"
                   placeholder="Book Location"
                   className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
@@ -65,6 +96,8 @@ const AddBook = () => {
             <label className="block mb-1">
               <span className="my-2">Writer</span>
               <input
+                onChange={handleOnchange}
+                name="writer"
                 type="text"
                 placeholder="Writer"
                 className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
@@ -73,7 +106,9 @@ const AddBook = () => {
             <label className="block mb-1">
               <span className="my-2">Publications</span>
               <input
-                type="number"
+                onChange={handleOnchange}
+                name="publications"
+                type="text"
                 placeholder="Publication"
                 className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
               />
@@ -81,6 +116,8 @@ const AddBook = () => {
             <label className="block mb-1">
               <span className="my-2">Edition</span>
               <input
+                onChange={handleOnchange}
+                name="edition"
                 type="text"
                 placeholder="Book Edition"
                 className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
@@ -89,6 +126,8 @@ const AddBook = () => {
             <label className="block mb-1">
               <span className="my-2">Pdf Link</span>
               <input
+                onChange={handleOnchange}
+                name="pdfLink"
                 type="text"
                 placeholder="Online pdf link"
                 className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
@@ -97,6 +136,8 @@ const AddBook = () => {
             <label className="block mb-1">
               <span className="my-2">Image Link</span>
               <input
+                onChange={handleOnchange}
+                name="image"
                 type="text"
                 placeholder="Book Image Link"
                 className="block w-full rounded-md shadow-sm bg-white py-2 px-2"
@@ -106,6 +147,7 @@ const AddBook = () => {
         </div>
         <div className="flex justify-center">
           <button
+            onClick={handleSubmit}
             type="button"
             className=" px-8 py-2 text-lg rounded bg-main text-white"
           >
