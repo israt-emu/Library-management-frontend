08f9c63f28c6677ff7804a83d91f061500bdee71
Tushar@8640 <imtantushar44@gmail.com>
2023-02-21 03:33:19 +0600

    notification added for user completed

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -205,16 +205,7 @@ const Navbar = ({ setToggle, toggle }) => {
                 onClick={() => setNotificationModal(!notificationModal)}
               >
                 {" "}
-                <button
-                  type="button"
-                  class="inline-block rounded bg-primary-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-                  data-te-modal-dismiss
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  Close
-                  <BsFillBellFill className="navbar-icon" />
-                </button>
+                <BsFillBellFill className="navbar-icon" />
               </button>
               <button
                 className="nav-icon dropdown-toggle"
@@ -230,7 +221,10 @@ const Navbar = ({ setToggle, toggle }) => {
           </div>
 
           {dropModal && <DropModal ref={ref} btnRef={btnRef} logOut={logOut} />}
-          <NotificationModal setNotificationModal={setNotificationModal} />
+          <NotificationModal
+            setNotificationModal={setNotificationModal}
+            notificationModal={notificationModal}
+          />
         </div>
       </header>
     </div>
--- src/components/modals/NotificationModal.js
+++ src/components/modals/NotificationModal.js
@@ -1,38 +1,70 @@
 import React from "react";
+import Moment from "react-moment";
+import {
+  useGetNoticesQuery,
+  useGetNotificationsQuery,
+  useUpdateNotificationStatusMutation,
+} from "../../features/notice/noticeApi";
 
+import { MdClose } from "react-icons/md";
 export default function NotificationModal({
   setNotificationModal,
   notificationModal,
 }) {
+  const [updateStatus, { isLoading, isError }] =
+    useUpdateNotificationStatusMutation();
+  const { data: notificationData } = useGetNotificationsQuery();
+  console.log(notificationData);
   return (
     <>
       {notificationModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="justify-end items-center flex overflow-x-hidden  fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative  max-w-sm  h-[calc(100%-6rem)] ">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none ">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t  h-[calc(100%-8rem)]  overflow-y-auto">
+                  <h3 className="text-3xl font-semibold">Notifications</h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="p-1 rounded-full ml-auto bg-main border-0 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setNotificationModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
+                    <span className=" outline-none focus:outline-none ">
+                     <MdClose className="text-white w-5 h-5"/>
                     </span>
                   </button>
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
+                  {notificationData?.notification?.map((d) => (
+                    <div className="mt-2 px-6 py-4 bg-white rounded-lg shadow w-full">
+                      <div className="inline-flex items-center justify-between w-full">
+                        <div className="inline-flex items-center">
+                          <img
+                            src="https://cdn-icons-png.flaticon.com/128/763/763812.png"
+                            alt="Training Icon"
+                            className="w-6 h-6 mr-3"
+                          />
+                          <h3 className="font-bold text-base text-gray-800">
+                            {d?.title}
+                          </h3>
+                        </div>
+                      </div>
+                      <p className="text-xs pl-8 text-gray-500 ">
+                        <Moment fromNow>{d?.createdAt}</Moment>
+                      </p>
+                      <p className="mt-1 text-sm pl-8">{d?.message}</p>
+                      <button
+                        disabled={isLoading || d?.read}
+                        onClick={() => updateStatus(d?._id)}
+                        className={`mt-2 text-sm ml-8 ${d?.read?"bg-gray-300":"bg-green-300"} px-3 py-1 flex justify-center items-center`}
+                      >
+                        {" "}
+                        Mark as Read
+                      </button>
+                    </div>
+                  ))}
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -43,13 +75,7 @@ export default function NotificationModal({
                   >
                     Close
                   </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setNotificationModal(false)}
-                  >
-                    Save Changes
-                  </button>
+            
                 </div>
               </div>
             </div>
